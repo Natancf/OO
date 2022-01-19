ed0656278961f21bdcf3eb3f6117b46b0acef4b4
Natancf <202017728@aluno.unb.br>
2022-01-19 14:56:42 -0300

    Primeiro commit java oo

--- teste/src/primeiro/MeuPrimeiroPrograma.java
+++ teste/src/primeiro/MeuPrimeiroPrograma.java
@@ -3,7 +3,7 @@ package primeiro;
 public class MeuPrimeiroPrograma {
 	
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
+		System.out.print("Hello, World!");
 		
 	}
 }
